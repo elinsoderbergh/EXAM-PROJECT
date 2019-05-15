8f56e23bf5a9c0ad4ccdc64af96cb36a0203f5a0
elinsoderbergh <47770325+elinsoderbergh@users.noreply.github.com>
2019-05-15 15:15:59 +0200

    Update recipeconstructor.js

--- recipeconstructor.js
+++ recipeconstructor.js
@@ -1,4 +1,4 @@
-//r
+/* creating a class early stage because we will use the properties several times. */
 class recipe {
 
     constructor(recipeTitle, recipeTime, recipeIngredients, recipeAllergies, recipeInstructions) {
@@ -10,41 +10,43 @@ class recipe {
         this.recipeInstructions = recipeInstructions;
     }
 
-    //all these three are methods of the class recipe, then need to be in the same function as the constructor!!
+//all these three are methods of the class recipe, then need to be in the same function as the constructor to be able to work
     generateHTMLStructure() {
         return  `
-        <div class="shop-item">
+        <div class='shop-item'>
                 <div>
-                    <span class="shop-item-title">${this.recipeTitle}</span> 
+                    <span class='shop-item-title'>${this.recipeTitle}</span> 
                 </div>
 
                 <div>
-                    <span class="shop-item-time">${this.recipeTime + ' min'}</span>
+                    <span class='shop-item-time'>${this.recipeTime + ' min'}</span>
                 <div>
-                    <span class="shop-item-ingredients">${'Ingredients: ' + this.recipeIngredients}</span>
+                    <span class='shop-item-ingredients'>${'Ingredients: ' + this.recipeIngredients}</span>
                 </div>
 
                 <div>
-                    <span class="shop-item-allergies"></span>
+                    <span class='shop-item-allergies'></span>
                 </div>
 
                 <div>
-                    <span class="shop-item-instructions">${'Instructions: ' + this.recipeInstructions}</span>
+                    <span class='shop-item-instructions'>${'Instructions: ' + this.recipeInstructions}</span>
                 </div>
                 
-                <div class="cart-column">
-                    <input class="cart-quantity-input" type="number" value="1"> 
-                        <button class="btn-shop" type="button">ADD TO SHOPPING LIST</button>       
+                <div class='cart-column'>
+                    <input class='cart-quantity-input' type='number' value='1'> 
+                        <button class='btn-shop' type='button'>ADD TO FAVORITES</button>       
                 </div>
-    </div>`
+        </div>`
     }
 
+    /* createHTML creates a block of code under the headline Favorites and when the button 'add to favorites' is clicked 
+    inin generateHTMlStructure, the block gets visible.*/
     createHTML() {    
-        var cartRecipes = document.getElementsByClassName('cart-recipes')[0]
-        var cartRow = document.createElement('div')
+        let cartRecipes = document.getElementsByClassName('cart-recipes')[0]
+        let cartRow = document.createElement('div')
         cartRow.classList.add('cart-row')
     
-        var cartRowContents = `
+        let cartRowContents = `
         <div class="cart-column"></div>
             <div>${this.recipeTitle}</div>
             <div>${this.recipeTime}</div>
@@ -58,10 +60,11 @@ class recipe {
         cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
 
     }
-    
+
+//displaying the recipes saved in local storage key 'myRecipes' on 'myPage'
     showHTML() { 
         return `
-        <div class="showRecipes">
+        <div class='showRecipes'>
             <div>
                 <h3>${this.recipeTitle}</h3>  
             </div>             
@@ -79,7 +82,8 @@ class recipe {
 
 }
 
-
+/* this function executes in the top of store.js to directly show the recipes on the websites
+the function contains all our recipes. */ 
 function getRecipes() { 
 recipes =
     [new recipe(
