efd600814e1ece482f794188d94114dfd8df2748
elinsoderbergh <47770325+elinsoderbergh@users.noreply.github.com>
2019-05-15 15:16:37 +0200

    Update user.js

--- user.js
+++ user.js
@@ -1,6 +1,8 @@
+/* created a early stage class because we want to use the properties several times. 
+stored it in a seperate file to be able to keep the structure */
 class User {
 
-    constructor(firstname, lastname, email, username, password) {  //nameing must match below, but could be anything. 
+    constructor(firstname, lastname, email, username, password) {  
         this.firstname = firstname;
         this.lastname = lastname;
         this.email = email;
