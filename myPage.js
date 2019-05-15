746cc9585163c7935b8d79bd6ede2e0515fb25a2
elinsoderbergh <47770325+elinsoderbergh@users.noreply.github.com>
2019-05-15 15:20:00 +0200

    Update myPage.js

--- myPage.js
+++ myPage.js
@@ -1,19 +1,12 @@
+//Functionality that displays the recipes stored in the local storage key 'myRecipes'
 
 function showRecipes() {  
-    console.log('test')
-    // if (localStorage.getItem('myRecipes') === null) { 
-    //     recipes = []; //since it's an empty squared bracket, the stored users from local storage comes in here.
-    //     localStorage.setItem('myRecipes', JSON.stringify(recipes));   
-    // } else {
     let recipes = JSON.parse(localStorage.getItem('myRecipes'));
     for (let i = 0; i < recipes.length; i++) {
         recipes[i] = new recipe(recipes[i].recipeTitle, recipes[i].recipeTime, recipes[i].recipeIngredients, recipes[i].recipeInstructions);
     }
 
-
     let html = ''
-
-
     for (let recipe of recipes) {
         html += recipe.showHTML();
     }
